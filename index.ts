365ce86e001679040eac62f8ac634bfd1aced336
Mykola Odnosumov <mkloouo@gmail.com>
2021-11-25 13:18:37 +0200

    fix teapot code

--- index.ts
+++ index.ts
@@ -3,24 +3,13 @@ import express from "express";
 const app = express();
 const port = 5050;
 
-app.get("/hello-world", (_req, res) => {
-  const status = 200;
-
-  return res.status(status).json({
-    string: "hello-world",
-    status,
-  });
-});
-
-app.get("/teapot", (_req, res) => {
+app.all("/teapot", (_req, res) => {
   const status = 418;
 
   return res.status(status).json({
-    string: "I'm a teapot",
+    message: "I'm a teapot",
     status,
   });
 });
 
-app.listen(port, () => {
-  console.log(`App is listening on localhost:${port}.`);
-});
+app.listen(port);
