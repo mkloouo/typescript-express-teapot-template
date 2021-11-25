ee8984eaee0113abccd332b8d4d49ce7d423170b
Mykola Odnosumov <mkloouo@gmail.com>
2021-11-25 20:00:09 +0300

    add helper message

--- index.ts
+++ index.ts
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 5050;
 
+app.all('/', (_req, res) => {
+  const status = 200;
+
+  return res.status(status).send('Try adding /teapot :)');
+});
+
 app.all("/teapot", (_req, res) => {
   const status = 418;
 
